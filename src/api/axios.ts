fa8b2dd700570c468cd6bc4a29ab5cf57f6b74ce
Zhao KK <kikakkz@hotmail.com>
2022-04-04 15:26:36 +0800

    Use singleton for axios meta

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,25 +1,42 @@
 import axios, { AxiosInstance } from "axios"
-import { useMetaRepoStore } from "../store/metarepo"
+
+class APIMeta {
+  private static meta: APIMeta
+
+  private baseURL: string
+  private appID: string
+
+  private constructor(baseURL?: string, appID?: string) {
+    this.baseURL = baseURL ? baseURL : ''
+    this.appID = appID ? appID : ''
+  }
+  public static new(baseURL?: string, appID?: string): APIMeta {
+    if (!this.meta) {
+      this.meta = new APIMeta(baseURL, appID)
+      return this.meta
+    }
+    return this.meta
+  }
+  public getBaseURL(): string {
+    return this.baseURL
+  }
+  public getAppID(): string {
+    return this.appID
+  }
+}
 
 const createAPI = (baseURL?: string, appID?: string): AxiosInstance | undefined => {
-  const metarepo = useMetaRepoStore()
-  if (baseURL) {
-    metarepo.BaseURL = baseURL
-  }
-  if (appID) {
-    metarepo.AppID = appID
-  }
-
-  if (metarepo.BaseURL.length === 0 || metarepo.AppID.length === 0) {
+  const meta = APIMeta.new(baseURL, appID)
+  if (meta.getBaseURL().length === 0 || meta.getAppID().length === 0) {
     return undefined
   }
 
   return axios.create({
     method: 'POST',
-    baseURL: process.env.DEV ? '/api' : metarepo.BaseURL,
+    baseURL: process.env.DEV ? '/api' : meta.getBaseURL(),
     headers: {
       'Content-Type': 'application/json',
-      'X-App-ID': metarepo.AppID
+      'X-App-ID': meta.getAppID()
     },
     withCredentials: true,
     responseType: 'json',

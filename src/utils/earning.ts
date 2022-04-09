86d03127066f09b6f346c612572a096f4ca104ce
Zhao KK <kikakkz@hotmail.com>
2022-04-09 12:27:31 +0800

    Use benefit

--- src/utils/earning.ts
+++ src/utils/earning.ts
@@ -3,13 +3,13 @@ import { useCurrencyStore, Currency } from '../store/currency'
 import { useBenefitStore, Benefit } from '../store/benefits'
 
 const totalEarningUSD = (done: (usdAmount: number) => void) => {
-  const benefits = useBenefitStore()
+  const benefit = useBenefitStore()
   const currencys = useCurrencyStore()
   const goods = useGoodStore()
 
   let amount = 0
 
-  benefits.forEach((benefit: Benefit) => {
+  benefit.Benefits.forEach((benefit: Benefit) => {
     const good = goods.getGoodByID(benefit.GoodID)
     if (!good) {
       return

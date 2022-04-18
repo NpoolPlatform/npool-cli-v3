0ae1fee12b20ab2db147b74b7b790f2ca213ce7f
Zhao KK <kikakkz@hotmail.com>
2022-04-18 11:38:00 +0800

    Export get application by id

--- src/store/church/applications/index.ts
+++ src/store/church/applications/index.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { Application } from '../../frontend'
 import { doActionWithError } from '../../action'
 import { API } from './const'
 import { ApplicationsState, GetApplicationsRequest, GetApplicationsResponse } from './types'
@@ -8,7 +9,13 @@ export const useApplicationsStore = defineStore('applications', {
     Applications: []
   }),
   getters: {
-    },
+    getApplicationByID (): (appID: string) => Application {
+      return (appID: string) => {
+        const index = this.Applications.findIndex((el) => el.App.ID === appID)
+        return index < 0 ? undefined as unknown as Application : this.Applications[index]
+      }
+    }
+  },
   actions: {
     getApplications (req: GetApplicationsRequest, done: (error: boolean) => void) {
       doActionWithError<GetApplicationsRequest, GetApplicationsResponse>(

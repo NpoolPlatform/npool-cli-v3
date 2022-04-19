805fdda183e5c0146a1e41f53c3cad3f51ef07ba
Zhao KK <kikakkz@hotmail.com>
2022-04-19 20:24:08 +0800

    Add fee apis

---
+++ src/store/church/fee/const.ts
@@ -0,0 +1,11 @@
+enum API {
+  CREATE_FEE_TYPE = '/cloud-hashing-goods/v1/create/fee/type',
+  UPDATE_FEE_TYPE = '/cloud-hashing-goods/v1/update/fee/type',
+
+  GET_FEES = '/cloud-hashing-goods/v1/get/fees',
+  CREATE_FEE = '/cloud-hashing-goods/v1/create/fee'
+}
+
+export {
+  API
+}

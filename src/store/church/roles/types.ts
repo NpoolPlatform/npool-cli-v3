75b44fc9d09385456d697775b7e7a0e7497c3553
Zhao KK <kikakkz@hotmail.com>
2022-04-19 12:50:17 +0800

    Fix create role user

--- src/store/church/roles/types.ts
+++ src/store/church/roles/types.ts
@@ -1,12 +1,11 @@
-import { BaseRequest } from '../../base'
-import { RoleUser, GetRoleUsersRequest, GetRolesRequest } from '../../admin'
+import { RoleUser, GetRoleUsersRequest, GetRolesRequest, CreateRoleUserRequest } from '../../admin'
 import { AppRole } from '../../frontend'
 
 interface AppRoleUser extends RoleUser {
   AppID: string
 }
 
-interface CreateAppRoleUserRequest extends BaseRequest {
+interface CreateAppRoleUserRequest extends CreateRoleUserRequest {
   TargetAppID: string
   Info: AppRoleUser
 }

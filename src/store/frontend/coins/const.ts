b8860e919d9194be222a308e35fc1fa21cdff531
Zhao KK <kikakkz@hotmail.com>
2022-04-19 18:15:39 +0800

    Use right api

--- src/store/frontend/coins/const.ts
+++ src/store/frontend/coins/const.ts
@@ -1,6 +1,6 @@
 enum API {
   GET_COINS = '/sphinx-coininfo/v1/get/coininfos',
-  GET_DESCRIPTION = '/sphinx-coininfo/v1/get/coin/description/by/coin'
+  GET_DESCRIPTION = '/sphinx-coininfo/v1/get/coin/description'
 }
 
 enum CoinDescriptionUsedFor {

c78e6a98575510ab806e2e6d417065eff3fb6541
Zhao KK <kikakkz@hotmail.com>
2022-05-01 20:59:49 +0800

    Add easy get api

--- src/store/frontend/stock/index.ts
+++ src/store/frontend/stock/index.ts
@@ -5,13 +5,21 @@ import { StockState } from './state'
 import {
   GetStocksRequest,
   GetStocksResponse,
+  Stock,
 } from './types'
 
 export const useStockStore = defineStore('stock', {
   state: (): StockState => ({
     Stocks: []
   }),
-  getters: {},
+  getters: {
+    getStockByGoodID (): (goodID: string) => Stock {
+      return (goodID: string) => {
+        const index = this.Stocks.findIndex((el) => el.GoodID === goodID)
+        return index < 0 ? undefined as unknown as Stock : this.Stocks[index]
+      }
+    }
+  },
   actions: {
     getStocks (req: GetStocksRequest, done: (error: boolean) => void) {
       doActionWithError<GetStocksRequest, GetStocksResponse>(

45ba7ae8fa68feef8df82c95ff35d5da231cf89a
Zhao KK <kikakkz@hotmail.com>
2022-07-14 21:53:59 +0800

    Support get coupon pools

--- src/store/admin/fixamount/const.ts
+++ src/store/admin/fixamount/const.ts
@@ -1,7 +1,7 @@
 enum API {
   CREATE_FIX_AMOUNT = '/cloud-hashing-inspire/v1/create/coupon/pool',
   UPDATE_FIX_AMOUNT = '/cloud-hashing-inspire/v1/update/coupon/pool',
-  GET_FIX_AMOUNTS = '/cloud-hashing-inspire/v1/get/coupon/pools'
+  GET_FIX_AMOUNTS = '/cloud-hashing-inspire/v1/get/coupon/pools/by/app'
 }
 
 export {

c032f0ce6ef253168c632a865fa71dfb6572a618
daqiang.li <dqli161@163.com>
2022-07-23 10:40:04 +0800

    edit referral path

--- src/store/admin/inspire/const.ts
+++ src/store/admin/inspire/const.ts
@@ -1,5 +1,5 @@
 enum API {
-  GET_USER_REFERRALS = '/cloud-hashing-apis-v2/v1/get/referrals/by/other/app/user',
+  GET_USER_REFERRALS = '/cloud-hashing-apis-v2/v1/get/user/referrals',
   GET_USER_GOOD_COMMISSIONS = '/cloud-hashing-apis-v2/v1/get/user/good/commissions'
 }
 

000fde3365ddf30f795dc1d59b293c2d5877bc2e
Zhao KK <kikakkz@hotmail.com>
2022-08-27 11:53:50 +0800

    Move user name to getter

--- src/store/base/appuser/index.ts
+++ src/store/base/appuser/index.ts
@@ -1,2 +1,30 @@
+import { defineStore } from 'pinia'
+import { User } from './types'
+
+export const useBaseUserStore = defineStore('base-user-v4', {
+  state: () => ({}),
+  getters: {
+    displayName (): (user: User, locale: string) => string {
+      return (user: User, locale: string) => {
+        let username = user.EmailAddress;
+        switch (locale) {
+          case 'ja-JP':
+            username =  user.FirstName + ' ' + user.LastName
+            break
+          default:
+            username = user.LastName + ' ' + user.FirstName
+            break
+        }
+        if (!username?.length) {
+            username = user.PhoneNO
+        }
+        return username
+      }
+    }
+  },
+  actions: {}
+})
+
+
 export * from './const'
 export * from './types'

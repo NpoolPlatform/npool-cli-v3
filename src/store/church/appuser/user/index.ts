c5780ca70749af5dd0da548d1e7a83d6120b4bbf
lidaqiang <dqli161@163.com>
2022-09-02 15:57:54 +0800

    Get User By AppID And UserID

--- src/store/church/appuser/user/index.ts
+++ src/store/church/appuser/user/index.ts
@@ -12,7 +12,15 @@ export const useChurchUserStore = defineStore('church-user-v3', {
   state: (): ChurchUserState => ({
     Users: new Map<string, Array<User>>()
   }),
-  getters: {},
+  getters: {
+    getUserByAppUserID (): (appID: string, userID: string) => User {
+      return (appID: string, userID: string) => {
+        const index = this.Users.get(appID)?.findIndex((el) => el.ID === userID)
+        const users = this.Users.get(appID) as Array<User>
+        return (index === undefined || index < 0) ? undefined as unknown as User : users[index]
+      }
+    }
+  },
   actions: {
     getAppUsers (req: GetAppUsersRequest, done: (users: Array<User>, error: boolean) => void) {
       doActionWithError<GetAppUsersRequest, GetAppUsersResponse>(

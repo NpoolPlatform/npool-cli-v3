e9859a17c8c13b4e921188bf6db82136bc350a12
Zhao KK <kikakkz@hotmail.com>
2022-09-04 17:28:39 +0800

    Add display name 1

--- src/store/base/appuser/index.ts
+++ src/store/base/appuser/index.ts
@@ -20,6 +20,24 @@ export const useBaseUserStore = defineStore('base-user-v4', {
         }
         return username
       }
+    },
+
+    displayName1 (): (emailAddress: string, phoneNO: string, firstName: string, lastName: string, locale: string) => string {
+      return (emailAddress: string, phoneNO: string, firstName: string, lastName: string, locale: string) => {
+        let username = emailAddress;
+        switch (locale) {
+          case 'ja-JP':
+            username =  firstName + ' ' + lastName
+            break
+          default:
+            username = lastName + ' ' + firstName
+            break
+        }
+        if (!username?.length) {
+            username = phoneNO
+        }
+        return username
+      }
     }
   },
   actions: {}

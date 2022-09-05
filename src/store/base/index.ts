033b913cdaf1ed960871aec7fdf3351c0912650a
lidaqiang <dqli161@163.com>
2022-09-05 16:23:23 +0800

    Add Base Order Type

--- src/store/base/index.ts
+++ src/store/base/index.ts
@@ -1,3 +1,4 @@
 export * from './appuser'
 export * from './notify'
 export * from './account'
+export * from './order'
---
+++ src/store/base/order/const.ts
@@ -0,0 +1,26 @@
+const OrderTimeoutSeconds = 6 * 60 * 60
+enum OrderType {
+  DefaultOrderType = 'DefaultOrderType',
+  Normal = 'Normal',
+  Offline = 'Offline',
+  Airdrop = 'Airdrop',
+}
+
+const OrderTypes = [
+  'ALL',
+  OrderType.Normal,
+  OrderType.Offline,
+  OrderType.Airdrop
+]
+
+enum OrderState {
+  DEFAULT_STATE = 'DefaultState',
+  WAIT_PAYMENT = 'WaitPayment',
+  PAID = 'Paid',
+  PAYMENT_TIMEOUT = 'PaymentTimeout',
+  USER_CANCELED = 'UserCanceled',
+  CANCELED = 'Canceled',
+  IN_SERVICE = 'InService',
+  EXPIRED = 'Expired',
+}
+export { OrderTimeoutSeconds, OrderState, OrderType, OrderTypes }

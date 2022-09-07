d8445d093b47838cfbe7fbfb9bcd81ceb8346755
lidaqiang <dqli161@163.com>
2022-09-07 17:29:49 +0800

    Add Update App API

--- src/store/church/appuser/app/index.ts
+++ src/store/church/appuser/app/index.ts
@@ -2,25 +2,56 @@ import { doActionWithError } from '../../../action'
 import { defineStore } from 'pinia'
 import { API } from './const'
 import { App } from '../../../base'
-import { GetAppsRequest, GetAppsResponse } from './types'
+import { CreateAppRequest, CreateAppResponse, GetAppsRequest, GetAppsResponse, UpdateAppRequest, UpdateAppResponse } from './types'
 
 export const useChurchAppStore = defineStore('church-app-v4', {
   state: () => ({
-    Apps: [] as Array<App>
+    Apps: {
+      Apps: [] as Array<App>,
+      Total: 0
+    }
+
   }),
   getters: {},
   actions: {
     getApps (req: GetAppsRequest, done: (apps: Array<App>, error: boolean) => void) {
       doActionWithError<GetAppsRequest, GetAppsResponse>(
-        API.GET_APPLICATIONS,
+        API.GET_APPS,
         req,
         req.Message,
         (resp: GetAppsResponse): void => {
-          this.Apps = resp.Infos
+          this.Apps.Apps.push(...resp.Infos)
+          this.Apps.Total = resp.Total
           done(resp.Infos, false)
         }, () => {
           done([], true)
         })
-    }
+    },
+    updateApp (req: UpdateAppRequest, done: (app: App, error: boolean) => void) {
+      doActionWithError<UpdateAppRequest, UpdateAppResponse>(
+        API.UPDATE_APP,
+        req,
+        req.Message,
+        (resp: UpdateAppResponse): void => {
+          const index = this.Apps.Apps.findIndex((el) => el.ID === resp.Info.ID)
+          this.Apps.Apps.splice(index, 1, resp.Info)
+          done(resp.Info, false)
+        }, () => {
+          done({} as App, true)
+        })
+    },
+    createApp (req: CreateAppRequest, done: (app: App, error: boolean) => void) {
+      doActionWithError<CreateAppRequest, CreateAppResponse>(
+        API.CREATE_APP,
+        req,
+        req.Message,
+        (resp: CreateAppResponse): void => {
+          this.Apps.Apps.push(resp.Info)
+          this.Apps.Total += 1
+          done(resp.Info, false)
+        }, () => {
+          done({} as App, true)
+        })
+    },
   }
 })
--- src/store/church/appuser/app/types.ts
+++ src/store/church/appuser/app/types.ts
@@ -1,4 +1,4 @@
-import { App, BaseRequest } from '../../../base'
+import { App, BaseRequest, RecaptchaType, SignMethodType } from '../../../base'
 
 export interface GetAppsRequest extends BaseRequest {
   Offset: number;
@@ -6,4 +6,42 @@ export interface GetAppsRequest extends BaseRequest {
 }
 export interface GetAppsResponse {
   Infos: Array<App>;
+  Total: number;
+}
+
+export interface UpdateAppRequest extends BaseRequest {
+  ID:                 string;
+  CreatedBy?:          string;
+  Name:               string;
+  Logo:               string;
+  Description:        string;
+  Banned?:             boolean;
+  BanMessage?:         string;
+  SignupMethods?:      Array<SignMethodType>;
+  ExtSigninMethods?:   Array<SignMethodType>;
+  RecaptchaMethod:    RecaptchaType;
+  KycEnable:          boolean;
+  SigninVerifyEnable: boolean;
+  InvitationCodeMust: boolean;
+}
+export interface UpdateAppResponse {
+  Info: App;
+}
+
+export interface CreateAppRequest extends BaseRequest {
+  CreatedBy?:          string;
+  Name:               string;
+  Logo:               string;
+  Description:        string;
+  Banned?:             boolean;
+  BanMessage?:         string;
+  SignupMethods?:      Array<SignMethodType>;
+  ExtSigninMethods?:   Array<SignMethodType>;
+  RecaptchaMethod:    RecaptchaType;
+  KycEnable:          boolean;
+  SigninVerifyEnable: boolean;
+  InvitationCodeMust: boolean;
+}
+export interface CreateAppResponse {
+  Info: App;
 }

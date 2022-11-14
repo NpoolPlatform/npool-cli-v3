855eb6221e74e4126b63d9b1d9e86f4d077aa587
lidaqiang <dqli161@163.com>
2022-11-14 17:17:48 +0800

    Wrong Path

--- src/store/church/account/user/const.ts
+++ src/store/church/account/user/const.ts
@@ -1,5 +1,5 @@
 export enum API {
   GET_APP_DEPOSITACCOUNTS = '/account/v1/get/app/depositaccounts',
   GET_N_APP_USERACCOUNTS = '/account/v1/get/n/app/useraccounts',
-  UPDATE_APP_USERACCOUNT = '/account/v1/get/update/app/user/useraccount'
+  UPDATE_APP_USERACCOUNT = '/account/v1/update/app/user/useraccount'
 }

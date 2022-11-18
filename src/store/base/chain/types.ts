776e7ef2c3e06e05e04d961dc66941a15d639667
lidaqiang <dqli161@163.com>
2022-11-18 10:32:01 +0800

    Add Base TX

--- src/store/base/chain/types.ts
+++ src/store/base/chain/types.ts
@@ -1,3 +1,5 @@
+import { AccountUsedFor } from '../account';
+
 export interface AppCoin {
   ID: string;
   AppID: string;
@@ -77,6 +79,50 @@ export interface CoinDescription {
   UsedFor: CoinUsedFor;
   Title: string;
   Message: string;
+  CreatedAt: number;
+  UpdatedAt: number;
+}
+
+/** @default "DefaultTxState" */
+export enum TxState {
+  DefaultTxState = "DefaultTxState",
+  StateCreated = "StateCreated",
+  StateWait = "StateWait",
+  StateTransferring = "StateTransferring",
+  StateSuccessful = "StateSuccessful",
+  StateFail = "StateFail",
+}
+
+/** @default "DefaultTxType" */
+export enum TxType {
+  DefaultTxType = "DefaultTxType",
+  TxWithdraw = "TxWithdraw",
+  TxFeedGas = "TxFeedGas",
+  TxPaymentCollect = "TxPaymentCollect",
+  TxBenefit = "TxBenefit",
+}
+
+export interface Tx {
+  ID: string;
+  CoinTypeID: string;
+  CoinName: string;
+  CoinLogo: string;
+  CoinUnit: string;
+  CoinENV: string;
+  AppID: string;
+  AppName: string;
+  FromAccountID: string;
+  FromAddress: string;
+  FromUsedFor: AccountUsedFor;
+  ToAccountID: string;
+  ToAddress: string;
+  ToUsedFor: AccountUsedFor;
+  Amount: string;
+  FeeAmount: string;
+  ChainTxID: string;
+  State: TxState;
+  Extra: string;
+  Type: TxType;
   CreatedAt: number;
   UpdatedAt: number;
 }

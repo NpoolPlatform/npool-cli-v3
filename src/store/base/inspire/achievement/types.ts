4c369c8694abc2ba3a4a11b50b5a83e2ae61fad9
lidaqiang <dqli161@163.com>
2022-11-29 10:02:38 +0800

    Add Base UserArchievement

---
+++ src/store/base/inspire/achievement/types.ts
@@ -0,0 +1,31 @@
+export interface GoodArchivement {
+  CoinTypeID: string;
+  CoinName: string;
+  CoinLogo: string;
+  CoinUnit: string;
+  GoodID: string;
+  GoodName: string;
+  GoodUnit: string;
+  CommissionPercent: number;
+  TotalUnits: number;
+  SelfUnits: number;
+  TotalAmount: string;
+  SelfAmount: string;
+  TotalCommission: string;
+  SelfCommission: string;
+  SuperiorCommission: string;
+}
+
+export interface UserArchivement {
+  UserID: string;
+  Username: string;
+  EmailAddress: string;
+  PhoneNO: string;
+  FirstName: string;
+  LastName: string;
+  CreatedAt: number;
+  InvitedAt: number;
+  Kol: boolean;
+  TotalInvitees: number;
+  Archivements: Array<GoodArchivement>;
+}

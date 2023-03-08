6e9c002f1596de7f6fb2af0f9cbbf822c75962ca
lidaqiang <dqli161@163.com>
2023-03-08 16:36:43 +0800

    add appdefaultgood api

---
+++ src/store/admin/good/appdefaultgood/index.ts
@@ -0,0 +1,67 @@
+import { defineStore } from 'pinia'
+import { AppDefaultGood } from '../../../base'
+import { doActionWithError } from '../../../action'
+import { API } from './const'
+import {
+  GetAppDefaultGoodsRequest,
+  GetAppDefaultGoodsResponse,
+  CreateAppDefaultGoodRequest,
+  CreateAppDefaultGoodResponse,
+  DeleteAppDefaultGoodRequest,
+  DeleteAppDefaultGoodResponse,
+} from './types'
+
+export const useAdminAppDefaultGoodStore = defineStore('admin-appdefaultgood-v4', {
+  state: () => ({
+    AppDefaultGoods: {
+      AppDefaultGoods: [] as Array<AppDefaultGood>,
+      Total: 0
+    }
+  }),
+  getters: {
+    getAppDefaultGoodByID () {
+      return (id: string) => this.AppDefaultGoods.AppDefaultGoods.find((el) => el.ID === id)
+    },
+  },
+  actions: {
+    getAppDefaultGoods (req: GetAppDefaultGoodsRequest, done: (rows: Array<AppDefaultGood>, error: boolean) => void) {
+      doActionWithError<GetAppDefaultGoodsRequest, GetAppDefaultGoodsResponse>(
+        API.GET_APP_DEFAULT_GOODS,
+        req,
+        req.Message,
+        (resp: GetAppDefaultGoodsResponse): void => {
+          this.AppDefaultGoods.AppDefaultGoods.push(...resp.Infos)
+          this.AppDefaultGoods.Total = resp.Total
+          done(resp.Infos, false)
+        }, () => {
+          done([], true)
+        })
+    },
+    createAppDefaultGood (req: CreateAppDefaultGoodRequest, done: (row: AppDefaultGood, error: boolean) => void) {
+      doActionWithError<CreateAppDefaultGoodRequest, CreateAppDefaultGoodResponse>(
+        API.CREATE_APP_DEFAULT_GOOD,
+        req,
+        req.Message,
+        (resp: CreateAppDefaultGoodResponse): void => {
+          const index = this.AppDefaultGoods.AppDefaultGoods.findIndex((el) => el.ID === resp.Info.ID)
+          this.AppDefaultGoods.AppDefaultGoods.splice(index < 0 ? 0 : index, index < 0 ? 0 : 1, resp.Info)
+          done(resp.Info, false)
+        }, () => {
+          done({} as AppDefaultGood, true)
+        })
+    },
+    deleteAppDefaultGood (req: DeleteAppDefaultGoodRequest, done: (row: AppDefaultGood, error: boolean) => void) {
+      doActionWithError<DeleteAppDefaultGoodRequest, DeleteAppDefaultGoodResponse>(
+        API.DELETE_APP_DEFAULT_GOOD,
+        req,
+        req.Message,
+        (resp: DeleteAppDefaultGoodResponse): void => {
+          const index = this.AppDefaultGoods.AppDefaultGoods.findIndex((el) => el.ID === resp.Info.ID)
+          this.AppDefaultGoods.AppDefaultGoods.splice(index, 1)
+          done(resp.Info, false)
+        }, () => {
+          done({} as AppDefaultGood, true)
+        })
+    }
+  }
+})
---
+++ src/store/admin/good/appdefaultgood/types.ts
@@ -0,0 +1,34 @@
+import { AppDefaultGood, BaseRequest } from '../../../base'
+
+export interface CreateAppDefaultGoodRequest extends BaseRequest{
+  AppID?: string;
+  GoodID: string;
+  CoinTypeID: string;
+}
+
+export interface CreateAppDefaultGoodResponse {
+  Info: AppDefaultGood;
+}
+
+export interface DeleteAppDefaultGoodRequest extends BaseRequest{
+  ID: string;
+  AppID?: string;
+}
+
+export interface DeleteAppDefaultGoodResponse {
+  Info: AppDefaultGood;
+}
+
+export interface GetAppDefaultGoodsRequest extends BaseRequest{
+  AppID?: string;
+  /** @format int32 */
+  Offset: number;
+  /** @format int32 */
+  Limit: number;
+}
+
+export interface GetAppDefaultGoodsResponse {
+  Infos: AppDefaultGood[];
+  /** @format int64 */
+  Total: number;
+}

b93fa94992efa19d0ce9a1aa304f78f06b45ef94
daqiangli <dqli161@163.com>
2023-05-04 15:21:14 +0800

    add round

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,6 +1,9 @@
 import { computed } from 'vue'
 
-export const getLocaleString = computed(() => (num: number | string) => {
-  const _num = Math.round(Number(num))
+export const getLocaleString = computed(() => (num: number | string, round?: boolean) => {
+  let _num = Number(num)
+  if (round) {
+    _num = Math.round(_num)
+  }
   return _num.toLocaleString()
 })

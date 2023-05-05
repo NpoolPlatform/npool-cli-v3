f6afb4a0ed88a1f4d20d99083a6894f10c6d21a6
daqiangli <dqli161@163.com>
2023-05-05 10:44:40 +0800

    update locale string

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,9 +1,15 @@
-import { computed } from 'vue'
+import { defineStore } from 'pinia'
 
-export const getLocaleString = computed(() => (num: number | string, round?: boolean) => {
-  let _num = Number(num)
-  if (round) {
-    _num = Math.round(_num)
+export const useLocaleStringStore = defineStore('local-localestring-v4', {
+  getters: {
+    getLocaleString () {
+      return (num: number | string, round?: boolean) => {
+        let _num = Number(num)
+        if (round) {
+          _num = Math.round(_num)
+        }
+        return _num.toLocaleString()
+      }
+    }
   }
-  return _num.toLocaleString()
 })

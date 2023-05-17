d0c9a8121210d5efc30e50b7173dab83c0696048
daqiangli <dqli161@163.com>
2023-05-17 13:17:26 +0800

    add min digits

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -3,12 +3,17 @@ import { defineStore } from 'pinia'
 export const useLocaleStringStore = defineStore('local-localestring-v4', {
   getters: {
     getLocaleString () {
-      return (num: number | string, round?: boolean) => {
+      return (num: number | string, round?: boolean, minDigits?: number) => {
         let _num = Number(num)
         if (round) {
           _num = Math.round(_num)
         }
-        return _num.toLocaleString("en-US", {maximumFractionDigits: 4 })
+
+        if (!minDigits) {
+          return _num.toLocaleString("en-US", {minimumFractionDigits: minDigits, maximumFractionDigits: 4 })
+        }
+        
+        return _num.toLocaleString("en-US", { maximumFractionDigits: 4 })
       }
     }
   }

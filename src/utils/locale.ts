520dcc35867a6644a6a13996c39018041882b771
daqiangli <dqli161@163.com>
2023-05-18 12:23:39 +0800

    change getLocaleString

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -3,14 +3,22 @@ import { defineStore } from 'pinia'
 export const useLocaleStringStore = defineStore('local-localestring-v4', {
   getters: {
     getLocaleString () {
-      return (num: number | string, round?: boolean, minDigits?: number) => {
+      return (num: number | string, round?: boolean, minDigits?: number, maxDigits?: number) => {
         let _num = Number(num)
         if (round) {
           _num = Math.round(_num)
         }
 
-        if (!minDigits) {
-          return _num.toLocaleString("en-US", {minimumFractionDigits: minDigits, maximumFractionDigits: 4 })
+        if (minDigits != undefined && maxDigits == undefined) {
+          return _num.toLocaleString("en-US", {minimumFractionDigits: minDigits })
+        }
+
+        if (maxDigits != undefined && minDigits == undefined) {
+          return _num.toLocaleString("en-US", { maximumFractionDigits: maxDigits })
+        }
+        
+        if (maxDigits != undefined && minDigits != undefined) {
+          return _num.toLocaleString("en-US", { minimumFractionDigits: minDigits, maximumFractionDigits: maxDigits })
         }
         
         return _num.toLocaleString("en-US", { maximumFractionDigits: 4 })

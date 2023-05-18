07ff4d1d2744cb77b67d99443311a43ef859de4a
daqiangli <dqli161@163.com>
2023-05-18 12:29:48 +0800

    delete round

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -3,11 +3,8 @@ import { defineStore } from 'pinia'
 export const useLocaleStringStore = defineStore('local-localestring-v4', {
   getters: {
     getLocaleString () {
-      return (num: number | string, round?: boolean, minDigits?: number, maxDigits?: number) => {
-        let _num = Number(num)
-        if (round) {
-          _num = Math.round(_num)
-        }
+      return (num: number | string, minDigits?: number, maxDigits?: number) => {
+        const _num = Number(num)
 
         if (minDigits != undefined && maxDigits == undefined) {
           return _num.toLocaleString("en-US", {minimumFractionDigits: minDigits })
